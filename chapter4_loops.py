fed2f58190168e82aa1b635a03870b808021b9b1
simon <simonzhang0428@gmail.com>
2020-05-07 12:09:38 -0700

    Create chapter4_loops.py

---
+++ chapter4_loops.py
@@ -0,0 +1,61 @@
+# P4.9
+# Write a program that reads a word and prints the word in reverse.
+# For example, if the user provides the input "Harry",
+# the program prints
+# yrraH
+
+
+# P4.12
+# Write a program that reads a word and prints all substrings, sorted by length.
+# For example, if the user provides the input "rum",
+# the program prints
+# r
+# u
+# m
+# ru
+# um
+# rum
+
+
+# P4.15
+# The Fibonacci numbers are defined by the sequence
+# f1 = 1
+# f2 = 1
+# fn = fn−1 + fn−2
+# Reformulate that as
+# fold1 = 1
+# fold2 = 1
+# fnew = fold1 + fold2
+# After that, discard fold2, which is no longer needed, and set fold2 to fold1 and fold1 to fnew.
+# Repeat an appropriate number of times.
+# Implement a program that prompts the user for an integer n and prints the nth Fibonacci number,
+# using the above algorithm.
+
+
+
+# P4.16
+# Factoring of integers.
+# Write a program that asks the user for an integer and
+# then prints out all its factors.
+# For example, when the user enters 150, the program should print
+# 2
+# 3
+# 5
+# 5
+
+
+
+# P4.17
+# Prime numbers.
+# Write a program that prompts the user for an integer and
+# then prints out all prime numbers up to that integer.
+# For example, when the user enters 20, the program should print
+#    2
+#    3
+#    5
+#    7
+#    11
+#    13
+#    17
+#    19
+# Recall that a number is a prime number if it is not divisible by any number except 1 and itself.
